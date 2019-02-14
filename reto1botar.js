c8c48ca39b53ffb3f5550d81c8c1774c8e4b77d5
Jorge27112 <47423961+Jorge27112@users.noreply.github.com>
2019-02-14 09:33:38 -0600

    Update reto1botar.js

--- reto1botar.js
+++ reto1botar.js
@@ -4,45 +4,46 @@ var y = 200
 var vely = 5
 
 function setup() {
-	createCanvas(600, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-	background(220);
-	mostrar();
-	moverx();
-	movery();
-	botarx();
-	botary();
+  background(220);
+  mostrar(200, 0, 0);
+  moverx();
+  movery();
+  botarx();
+  botary();
 }
 
-function mostrar() {
-	ellipse(x, y, 50, 50);
+function mostrar(R, G, B, tam) {
+  fill(R, G, B);
+  ellipse(x, y, 50, 50);
 }
 
 function moverx() {
-	x = x + velx;
+  x = x + velx;
 }
 
 
 function movery() {
-y=y+vely;
+  y = y + vely;
 }
 
 function botarx() {
-	if (x < 25) {
-		velx = velx * -1;
-	}
-	if (x > width-25) {
-		velx = velx * -1;
-	}
+  if (x < 25) {
+    velx = velx * -1;
+  }
+  if (x > width - 25) {
+    velx = velx * -1;
+  }
 }
 
 function botary() {
-if (y < 25) {
-		vely = vely * -1;
-	}
-	if (y > height-25) {
-		vely = vely * -1;
-	}
+  if (y < 25) {
+    vely = vely * -1;
+  }
+  if (y > height - 25) {
+    vely = vely * -1;
+  }
 }
